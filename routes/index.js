fb69e09304a79b7e7448b9fa13ebf7ae391531aa
bayuaslamaa <bayuaslamaa@gmail.com>
2020-04-10 23:15:43 +0700

    fix main route

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 const router = require("express").Router()
 const userRoutes = require("./user")
 const projectRoutes = require("./project")
-const auth = require("../middlewares/authentication")
 const taskRoutes = require("./task")
 
 
-// router.get("/", auth, (req, res) => {
-//     res.send("welcome to my humble app")
-// })
+router.get("/", (req, res) => {
+    res.send("welcome to my humble app")
+})
 
 router.use(userRoutes)
 router.use("/projects", projectRoutes)
